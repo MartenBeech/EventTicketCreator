0adade2d7320862eb4493398496dbb0d26dc8e3e
MartenBeech <mortenbech22@hotmail.com>
2023-05-04 11:03:32 +0200

    Updated UX

---
+++ src/components/Snackbar.tsx
@@ -0,0 +1,33 @@
+import { View, Text } from "react-native";
+import { Snackbar as Snack } from "react-native-paper";
+
+export type SnackbarColor = "green" | "red";
+
+interface Props {
+  textState: string;
+  setTextState: React.Dispatch<React.SetStateAction<string>>;
+  backgroundColor: SnackbarColor;
+}
+
+export const Snackbar = (props: Props) => {
+  return (
+    <Snack
+      visible={!!props.textState}
+      onDismiss={() => props.setTextState("")}
+      style={{
+        backgroundColor:
+          props.backgroundColor === "green" ? "#288139" : "#d82c2c",
+      }}
+      wrapperStyle={{ height: 0, top: 0, zIndex: 1 }}
+      duration={5000}
+      action={{
+        label: "Dismiss",
+        onPress: () => props.setTextState(""),
+      }}
+    >
+      <View>
+        <Text>{props.textState}</Text>
+      </View>
+    </Snack>
+  );
+};
